44798943667622401f712d2f8fefa836d7b2e28e
shadinvelikiy <buhbuhbabah@gmail.com>
2025-08-06 17:06:41 +0300

    [UI, Header] add - добавить компонент иконок, резиновый Header, модули для адаптива, сами иконки

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+</script>
+
 <template>
-    <div>Header.vue</div>
+  <div class="fixed w-full h-[86px] bg-white flex items-center justify-between px-4 md:px-[41px] z-50">
+
+    <div class="flex items-center gap-x-[16px]" :class="{'invisible': !isMobile()}">
+      <ui-icon name="user" />
+      <ui-icon name="search" />
+    </div>
+    
+    <div class="absolute left-1/2 transform -translate-x-1/2">
+      <img src="/logo.svg" alt="logo" class="h-8 md:h-10">
+    </div>
+
+    <div class="flex items-center">
+      <div v-show="!isMobile()" class="flex items-center gap-x-[16px]">
+        <ui-icon name="search" />
+        <ui-icon name="stroke" />
+        <ui-icon name="user" />
+      </div>
+      
+      <ui-icon name="basket" class="ml-[16px]" />
+    </div>
+  </div>
 </template>
---
+++ components/ui/Icon.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  name: { type: String, required: true },
+  size: { type: Number, default: 20 }
+})
+
+const iconContent = ref('')
+
+import(`~/assets/icons/${props.name}.svg?raw`).then(module => {
+  console.log(module)
+  iconContent.value = module.default
+})
+</script>
+
+<template>
+  <div 
+    class="cursor-pointer"
+    :style="{
+      width: size + 'px',
+      height: size + 'px',
+      display: 'inline-block'
+    }"
+    v-html="iconContent"
+  />
+</template>

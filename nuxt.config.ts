c60a6fa855d0af06cad9f92d7d8d756c9dd7508a
shadinvelikiy <buhbuhbabah@gmail.com>
2025-08-07 15:53:25 +0300

    for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
+  app: {
+    baseURL: '/stik-test/',
+  },
+
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-viewport'],
 
